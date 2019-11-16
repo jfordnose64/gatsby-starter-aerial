1a6eafeb33e7b4513ea0cab57e6ee49adbad236c
jackson ford <jfordnose64@gmail.com>
2019-11-16 11:52:17 -0500

    added heahshot

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import config from '../../config';
+import './index.css';
+import headshot from '../assets/images/headshot.jpg';
+
 export default function Footer() {
   return (
     <header id="header">
+      <img src={headshot} class="headshot" />
       <h1>{config.authorName}</h1>
       <p>{config.authorHeading}</p>
       <nav>
